aedab21662d40d6558f7acf6c5c66014a571c5b8
festad <d.festa004@studenti.unibs.it>
2021-11-15 08:00:47 +0000

    cleaning and adding garnizon column

--- health-information-systems/data-poland/r/tidied_zgonem_garnizon_plot.R
+++ health-information-systems/data-poland/r/tidied_zgonem_garnizon_plot.R
@@ -2,12 +2,18 @@ library(ggplot2)
 library(tidyverse)
 library(modelr)
 
-zg <- read_csv("tidy_2001-2020-niezgonem.csv") %>% filter(Year >= 2001)
+zg <- read_csv("tidy_zgonem-cities.csv") %>% 
+  select(Year, Garnizon, Suicides)       %>% 
+  filter(Year >= 2013)                   %>%
+  filter(Garnizon == "KSP Warszawa" | Garnizon == "KWP Katowice" |
+         Garnizon == "KWP Kraków" | Garnizon == "KWP Opole")
 
 plot <- ggplot(data = zg, mapping = aes(x = Year, y = Suicides)) +
-  geom_point(mapping = aes(color = Sex)) # +
+  geom_point(mapping = aes(color = Garnizon)) # +
 # geom_smooth(data = zg %>% filter(Sex == "Male"))  +
 # geom_smooth(data = zg %>% filter(Sex == "Female"))
+
+plot
 
 pred_m = lm(Suicides ~ Year, data = zg %>% filter(Sex == "Male"))
 pred_f = lm(Suicides ~ Year, data = zg %>% filter(Sex == "Female"))
@@ -19,7 +25,7 @@ plot <- plot +
   pred %>% geom_line(mapping = aes(x = Year, y = pred_m), colour = "blue")
 
 plot <- plot + labs(x = "Years",
-                    y = "Attempted suicides",
-                    title = "Attempted suicides in Poland")
+                    y = "Fatal suicides",
+                    title = "Fatal suicides in Poland")
 
 plot
