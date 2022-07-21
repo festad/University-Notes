771dca165635d25dee530bb4ef67bd4cf1a0963e
festad <d.festa004@studenti.unibs.it>
2022-07-21 15:53:01 +0200

    Sample plot with colour and shape

--- his/data-poland/r/db/plotter.R
+++ his/data-poland/r/db/plotter.R
@@ -3,14 +3,26 @@ library(ggplot2)
 
 dw <- read_csv("poland_suicides_full_database.csv") 
 
-dw %>% 
-  filter(Garnizon == 'KWP Kielce') %>%
-  filter(Year > 2013) %>%
-  filter(Result == 'Attempted') -> dw
-
 dw %>%
-  ggplot(mapping=aes(x=Year,y=`Suicides by sex`)) +
-  geom_point(mapping=aes(color=Sex)) +
-  geom_smooth(data = dw %>% filter(Sex == 'Male')) +
-  geom_smooth(data = dw %>% filter(Sex == 'Female'))
-  
+  filter(Garnizon == 'Polska') %>%
+  filter(Year > 1998) -> dw
+
+plt <- ggplot(dw, mapping=aes(x=Year,y=`Suicides by sex`)) +
+geom_point(mapping=aes(color=Sex, shape=Result)) +
+labs(
+  title = "Suicides in Poland",
+  subtitle = "(1999-2021)",
+  caption = "Data from statystyczna ...",
+  x = "Year",
+  y = "Suicides",
+  colour = "Sex",
+  shape = "Fatality"
+)
+
+plt <- plt + theme_dark()
+
+saving <- function(plt, name) {
+  ggsave(name, plt, device="eps")
+}
+
+saving(plt, "plot_bla")
