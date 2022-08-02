990c1cc2a5bf224ab739f9f990a4edc87c99cbae
festad <d.festa004@studenti.unibs.it>
2022-08-02 19:36:59 +0200

    Removing and moving

--- dip/convolutions/driver_convolution.py
+++ dip/convolutions/driver_convolution.py
@@ -16,3 +16,24 @@ dx = cvl.convolution2d(img,kerrdx,verbose=True)
 print(f'\nDx:\n{dx}\n')
 dy = cvl.convolution2d(img,kerrdy,verbose=True)
 print(f'\nDy:\n{dy}\n')
+
+print('\nWith Fourier:\n')
+t_img = np.fft.fft2(img)
+
+def padd_kerr(kerr, img_h, img_l):
+    res = np.zeros(shape=(img_h,img_l),
+                   dtype=kerr.dtype)
+    res[:kerr.shape[0],:kerr.shape[1]] = kerr
+    return res
+
+kerrdx = padd_kerr(kerrdx,img.shape[0],img.shape[1])
+kerrdy = padd_kerr(kerrdy,img.shape[0],img.shape[1])
+    
+t_kdx = np.fft.fft2(kerrdx)
+t_kdy = np.fft.fft2(kerrdy)
+
+f_dx = np.fft.ifft2(t_img*t_kdx)
+f_dy = np.fft.ifft2(t_img*t_kdy)
+
+print(f'Dx:\n{f_dx}\n')
+print(f'Dy:\n{f_dy}\n')
